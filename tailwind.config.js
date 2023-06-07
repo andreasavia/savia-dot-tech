87ea75178a0e30d358f84f7e6b03c7ad662b0ed3
Andrea Savia <asavia3@gmail.com>
2023-06-07 23:47:54 +0100

    Add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'theblack': '#000000',
+      'thewhite': '#FFFFFF',
+      'thespark': '#BEF754',
+    },
     extend: {},
   },
   plugins: [
     require('prettier-plugin-tailwindcss')
   ],
 }
-
